7e65ec744f5cad26af23dccd0637eb32eebb57d2
doraemonfanclub2023-web <doraemonfanclub2023@gmail.com>
2026-07-03 22:30:54 +0700

    Delete cloudinary-config.js

--- cloudinary-config.js
+++
@@ -1,9 +0,0 @@
-// js/cloudinary-config.js
-
-export const cloudinaryConfig = {
-    cloudName: "gntkeeno",
-    uploadPreset: "Doraemon fansub Việt Nam" // Tên preset bồ vừa tạo thành công lúc nãy
-};
-
-// Đường dẫn API để lát nữa code JS của mình tự gọi lên Cloudinary để upload
-export const CLOUDINARY_URL = `https://api.cloudinary.com/v1_1/${cloudinaryConfig.cloudName}/upload`;
