31567794279af79d045600eea4c8ac06b881301f
doraemonfanclub2023-web <doraemonfanclub2023@gmail.com>
2026-07-04 22:13:08 +0700

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,4 +1,3 @@
-// Dùng link CDN trực tiếp cho Web thuần
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
@@ -7,9 +6,11 @@ const firebaseConfig = {
   apiKey: "AIzaSyBt3G9n5JYu3EsvqJR9IoW2vRAc_Es3-ws",
   authDomain: "doraemon-fansub-vietnam.firebaseapp.com",
   projectId: "doraemon-fansub-vietnam",
-  storageBucket: "doraemon-fansub-vietnam.appspot.com", // Đã sửa lại đúng đường dẫn chuẩn
+  // SỬA ĐÚNG DÒNG NÀY THEO CẤU HÌNH CỦA FIREBASE
+  storageBucket: "doraemon-fansub-vietnam.firebasestorage.app", 
   messagingSenderId: "380840935390",
-  appId: "1:380840935390:web:60fab4722a9fba5053a74f"
+  appId: "1:380840935390:web:60fab4722a9fba5053a74f",
+  measurementId: "G-0S2Z4XRK0B"
 };
 
 const app = initializeApp(firebaseConfig);
